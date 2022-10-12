3a061c6a15a30c129a694ce6e22fcb982f8e4d28
emachipana <emachipana0102@gmail.com>
2022-10-11 23:55:18 -0500

    ADD: apply global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { reset } from './styles';
+import { Global } from "@emotion/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global styles={reset} />
     <App />
   </React.StrictMode>
 );
