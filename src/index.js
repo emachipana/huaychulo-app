e537d7395e905ec71443f6d770d5ee66b062ed74
emachipana <emachipana0102@gmail.com>
2022-10-12 23:15:46 -0500

    ADD: involved app in browser router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { reset } from './styles';
 import { Global } from "@emotion/react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Global styles={reset} />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
