b093344d90ed00d3162d64c915ba7300e678dfef
emachipana <emachipana0102@gmail.com>
2022-10-24 11:41:45 -0500

    ADD: section on pages style

---
+++ src/pages/client/styles.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.main`
+  margin: 0 2rem;
+  overflow: hidden;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  flex-wrap: wrap;
+  margin: 5rem auto;
+`;
