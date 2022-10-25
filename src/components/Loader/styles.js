1e97650707be23d39a3e0a7e493ecfa553dc81ae
emachipana <emachipana0102@gmail.com>
2022-10-25 11:31:01 -0500

    ADD: loader component

---
+++ src/components/Loader/styles.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Backdrop = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, .6);
+`;
