3c385fc0812daca69fb6fd09d94905b3853f2da5
emachipana <emachipana0102@gmail.com>
2022-10-26 23:04:32 -0500

    ADD: Container to admin pages

--- src/AuthAppAdmin.js
+++ src/AuthAppAdmin.js
@@ -1,23 +1,34 @@
+import styled from "@emotion/styled";
 import { Route, Routes } from "react-router-dom";
 import AsideNav from "./components/AsideNav";
 import NavBarAdmin from "./components/NavBarAdmin";
+import DishesPage from "./pages/admin/dishes";
 
 function AuthAppAdmin() {
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  `;
+
+  const Section = styled.section`
+    position: relative;
+    height: 100%;
+    padding: 2rem 1rem;
+  `;
+
   return (
     <div style={{display: "flex"}}>
       <AsideNav />
-      <div style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "100%"
-      }}>
-        <NavBarAdmin 
-          orders={["s"]}
-        />
-        <Routes>
-          <Route index path="/" element={<h1>Hola desde admin</h1>} />
-        </Routes>
-      </div>
+      <Container>
+        <NavBarAdmin />
+        <Section>
+          <Routes>
+            <Route index path="/" element={<h1>Hola desde admin</h1>} />
+            <Route path="/platillos" element={<DishesPage />} />
+          </Routes>
+        </Section>
+      </Container>
     </div>
   );
 }
