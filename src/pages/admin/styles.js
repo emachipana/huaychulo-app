194f0018e6b4e9edb9a31f2a17be89c504fed972
emachipana <emachipana0102@gmail.com>
2022-10-26 23:00:43 -0500

    ADD: styles to admin pages

---
+++ src/pages/admin/styles.js
@@ -0,0 +1,36 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
+export const FlexRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin: 2rem auto;
+`;
+
+export const Categories = styled.div`
+  overflow: auto;
+  width: 100%;
+  max-height: 150px;
+  z-index: 1;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 2rem;
+  flex-wrap: wrap;
+`;
+
+export const LoaderStyle = css`
+  position: fixed;
+  top: 50%;
+  right: 40%;
+`;
