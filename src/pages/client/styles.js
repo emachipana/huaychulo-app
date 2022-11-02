53bd2215f8a951da23b74ccb8b98e8050e52a46c
emachipana <emachipana0102@gmail.com>
2022-11-02 13:36:39 -0500

    ADD: styles to about page

--- src/pages/client/styles.js
+++ src/pages/client/styles.js
@@ -1,3 +1,4 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
 export const Container = styled.main`
@@ -18,4 +19,53 @@ export const Title = styled.h2`
   font-size: 2rem;
   font-weight: 700;
   margin: 0 5%;
+`;
+
+export const SwiperStyle = css`
+  width: 100%;
+  padding-top: 20px;
+  padding-bottom: 40px;
+
+  .swiper-slide {
+    background-position: center;
+    background-size: cover;
+    width: 480px;
+    border-radius: 1rem;
+    height: 400px;
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+    border-radius: 1rem;
+  }
+
+  @media screen and (max-width: 550px) {
+    .swiper-slide {
+      width: 350px;
+      height: 250px;
+    }
+
+    .swiper-slide img {
+      height: 250px;
+    }
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  gap: 3rem;
+  flex-wrap: wrap;
+  margin: 2rem 1rem;
+  padding: 1rem;
+`;
+
+export const Text = styled.p`
+  text-align: center;
+  font-size: 1rem;
+  font-weight: 500;
 `;
